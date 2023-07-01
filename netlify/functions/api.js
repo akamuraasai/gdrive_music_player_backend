bae9799390092b5cddeb16d1b9dd78a42bde8e66
Jonathan Willian <jonathan.willian.tod@gmail.com>
2023-07-01 04:22:51 +0000

    fix: added cors and bodyparser configs to netlify.

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,4 +1,6 @@
 import express, { Router } from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 import serverless from 'serverless-http';
 import world from '../../src/charts/world';
 import genreWorld from '../../src/charts/genreWorld';
@@ -9,6 +11,8 @@ import songDetails from '../../src/tracks/details';
 import songRelated from '../../src/tracks/related';
 
 const api = express();
+api.use(bodyParser.json());
+api.use(cors());
 
 const router = Router();
 
